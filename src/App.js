982b938bae5b0a853020dce407f56ce7822cb0fb
Rimpei <rin1220popo@gmail.com>
2022-09-03 16:44:07 -0700

    completed dnd with dummy data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import Main from "./components/Main";
 
 function App() {
   return (
     <div style={{ padding: "50px" }}>
       <h1 style={{ marginBottom: "20px" }}>Trello-like TODO App</h1>
+      <Main />
     </div>
   );
 }
--- src/dummyData.js
+++ src/dummyData.js
@@ -1,6 +1,6 @@
 import { v4 as uuidv4 } from "uuid";
 
-dummyData = [
+const dummyData = [
     {
         id: uuidv4(),
         title: "Todo",
